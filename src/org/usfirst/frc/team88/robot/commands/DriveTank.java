fd4590c636593b1b01cdccd0820158d07981cc91
tj2 <navtges@gmail.com>
2016-12-01 20:36:22 -0500

    Works for a 6 motor base

--- src/org/usfirst/frc/team88/robot/commands/DriveTank.java
+++ src/org/usfirst/frc/team88/robot/commands/DriveTank.java
@@ -10,12 +10,23 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DriveTank extends Command {
 	private double left;
 	private double right;
+	private int state;
+	private boolean lowGear;
+	private int lastShift;
+	private double speed;
+	private static final int DRIVING = 1;
+	private static final int PREP = 2;
+	private static final int SHIFT = 3;
+	private static final double SHIFTSPEED = 7.0;
 	
     public DriveTank() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.drive);
     	requires(Robot.oiNetTable);
+    	state = DRIVING;
+    	lowGear = true;
+    	
     }
 
     // Called just before this Command runs the first time
@@ -24,17 +35,42 @@ public class DriveTank extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	left = Robot.oi.getDriverLeftVerticalAxis();
-    	right = Robot.oi.getDriverRightVerticalAxis();
+
     	
-    	left = Robot.oi.applyDeadZone(left);
-    	right = Robot.oi.applyDeadZone(right);
-    	
-    	Robot.oiNetTable.table.putDouble("Right Current", Robot.drive.getRightCurrent());
-    	Robot.oiNetTable.table.putDouble("Left Current", Robot.drive.getLeftCurrent());
-    	
-    	Robot.drive.tankDrive(-left, right);
-    	
+    	switch (state){
+    	case DRIVING:
+        	left = Robot.oi.getDriverLeftVerticalAxis();
+        	right = Robot.oi.getDriverRightVerticalAxis();
+        	
+        	left = Robot.oi.applyDeadZone(left);
+        	right = Robot.oi.applyDeadZone(right);
+        	
+        	Robot.oiNetTable.table.putDouble("Right Current", Robot.drive.getRightCurrent());
+        	Robot.oiNetTable.table.putDouble("Left Current", Robot.drive.getLeftCurrent());
+        	
+        	speed = Robot.drive.getAvgSpeed();
+        	Robot.drive.tankDrive(left, right);
+        	lastShift++;
+        
+        	//Comment out in order to use open loop and set the state to permanent drive
+//        	if((lastShift > 200) && 
+//        			((speed > SHIFTSPEED && lowGear)||
+//        			(speed < SHIFTSPEED && !lowGear))){
+//        		state = PREP;
+//        	}
+    		break;
+    	case PREP:
+    		left = Robot.drive.getLeftSpeed();
+    		right = Robot.drive.getRightSpeed();
+    		Robot.drive.tankDrive(left, right);
+    		
+    		state = SHIFT;
+    		break;
+    	case SHIFT:
+    		
+    		lastShift = 0;
+    		break;
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -7,6 +7,8 @@ import org.usfirst.frc.team88.robot.commands.DriveArcade;
 import org.usfirst.frc.team88.robot.commands.DriveTank;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -18,25 +20,35 @@ public class Drive extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
-	private final CANTalon lTalon, lTalonSlave, rTalon, rTalonSlave;
+	private final CANTalon lTalon, lTalonSlave, lTalonSlave2, rTalon, rTalonSlave, rTalonSlave2;
 	private final RobotDrive robotDrive;
+	private DoubleSolenoid shifter;
 
 	public Drive() {
 		lTalon = new CANTalon(RobotMap.driveLeft);
 		lTalonSlave = new CANTalon(RobotMap.driveLeftSlave);
 		lTalonSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
 		lTalonSlave.set(lTalon.getDeviceID());
+		lTalonSlave2 = new CANTalon(RobotMap.driveLeftSlave2);
+		lTalonSlave2.changeControlMode(CANTalon.TalonControlMode.Follower);
+		lTalonSlave2.set(lTalon.getDeviceID());
 		
 		rTalon = new CANTalon(RobotMap.driveRight);
 		rTalonSlave = new CANTalon(RobotMap.driveRightSlave);
 		rTalonSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
 		rTalonSlave.set(rTalon.getDeviceID());
+		rTalonSlave2 = new CANTalon(RobotMap.driveRightSlave2);
+		rTalonSlave2.changeControlMode(CANTalon.TalonControlMode.Follower);
+		rTalonSlave2.set(rTalon.getDeviceID());
+		
+		shifter = new DoubleSolenoid(RobotMap.shifterSolenoidLow, RobotMap.shifterSolenoidHigh);
+		shifter.set(Value.kForward);
 		
 		robotDrive = new RobotDrive(lTalon, rTalon);
 	}
 
 	public void tankDrive(double left, double right)  {
-		robotDrive.tankDrive(left, -right);
+		robotDrive.tankDrive(left, right);
 	}
 	
 	public void arcadeDrive() {
@@ -49,6 +61,19 @@ public class Drive extends Subsystem {
 	
 	public double getRightCurrent(){
 		return rTalon.getOutputCurrent();
+	}
+	
+	public double getLeftSpeed(){
+		return lTalon.getSpeed();
+	}
+	
+	public double getRightSpeed(){
+		return rTalon.getSpeed();
+	}
+	
+	public double getAvgSpeed(){
+		double speed = (rTalon.getSpeed() + lTalon.getSpeed()) / 2;
+		return speed;
 	}
 	
     public void initDefaultCommand() {
